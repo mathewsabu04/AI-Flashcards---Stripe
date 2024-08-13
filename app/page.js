d1056fa1360639f4756e28ea84ce1bbb09e22419
JasonCheng31 <chengjcrp831@gmail.com>
2024-08-12 22:05:01 -0400

    Left at 30:03

--- app/page.js
+++ app/page.js
@@ -1,8 +1,68 @@
 import Image from "next/image";
-
+import getStripe from '@/utils/get-stripe'
+import {SignedIn,SignedOut, UserButton} from '@clerk/nextjs'
+import {AppBar, Box, Button, Container, Grid, Toolbar, Typography} from '@mui/material'
+import Head from 'next/head'
 
 export default function Home() {
-  return 
-    
-  
+  return (
+    <Container maxWidth="100vw">
+      <Head>
+        <title>FlashCard SaaS</title>
+        <meta name = "description" content = 'Create flashcard from your text' />
+      </Head>
+
+      <AppBar position ="static">
+        <Toolbar>
+          <Typography variant = "h6" style= {{flexGrow: 1}}>Flashcard SaaS</Typography>
+          <SignedOut>
+            <Button color ="inherit">Login</Button>
+            <Button color ="inherit">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton></UserButton>
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{
+        textAlign: 'center',
+        my: 4,
+      }}
+      >
+        <Typography variant ="h2">Welcome to Flashcard SaaS</Typography>
+        <Typography variant = "h5">
+          {' '}
+          The easiest way to make flashcards from your text
+        </Typography>
+        <Button variant ='contained' color ='primary' sx= {{mt: 2}}>Get Started</Button>
+      </Box>
+      <Box sx={{my: 6}}>
+        <Typography variant = "h2" components="h2">
+          Features
+        </Typography>
+       
+        <Grid contained spacing ={4}>
+          <Grid item xs={12} md={4}>
+          <Typography variant ="h6">Easy Text Input</Typography>
+          <Typography>
+            {' '}
+            Just input your text and let our software do the rest.</Typography>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant ="h6">Smart FlashCards</Typography>
+          <Typography>
+          {' '}
+            Simply input your text and let our software do the rest</Typography>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant ="h6">Easy Access</Typography>
+          <Typography>
+          {' '}
+            Simply input your text and let our software do the rest</Typography>
+        </Grid>
+        </Grid>
+      </Box>
+</Container>
+  )
 }
